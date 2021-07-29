5bab1d40d4408bf29c21cf0d23239a36cc950294
qreenify <adam@tapper.one>
2021-07-29 10:26:21 +0200

    enemy prefab created

--- Assets/Scripts/SoundButton.cs
+++ Assets/Scripts/SoundButton.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void OnEnable()
+   {
+      
+   }
 }
