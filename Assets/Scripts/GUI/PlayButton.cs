c334ac77eaa62ccc999370ad229a0069f6e01cc7
qreenify <adam@tapper.one>
2021-08-03 10:08:51 +0200

    Healthbar added

--- Assets/Scripts/GUI/PlayButton.cs
+++ Assets/Scripts/GUI/PlayButton.cs
@@ -7,6 +7,6 @@ public class PlayButton : MonoBehaviour
 {
     public void Play()
     {
-        GetComponentInParent<Scene>()
+        GetComponentInParent<Scene>();
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName;
+    public LoadSceneMode mode = LoadSceneMode.Additive;
+    public bool onAwake = true;
+    private void Awake()
+    {
+        if (onAwake)
+            LoadScene();
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName, mode);
+    }
+}
